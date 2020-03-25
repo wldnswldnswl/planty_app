e331d161a29d06a5ada8a6b8c8278759a599a38f
LEEJIUN <wldnswldnslw@naver.com>
2020-03-25 16:39:13 +0900

    Config Serverless Framework

--- App.js
+++ App.js
@@ -8,10 +8,13 @@
 
 import React from 'react';
 import AppStack from './src/screens/index';
+import Amplify from 'aws-amplify';
+import amplify from './aws-exports';
 
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  Amplify.configure(amplify);
   return (
       <AppStack />
   )
