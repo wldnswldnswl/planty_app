740cfedb0a6d9424ed76cea8d163606a0743be04
LEEJIUN <wldnswldnslw@naver.com>
2020-03-29 14:04:48 +0900

    Revert "Config Serverless Framework"
    This reverts commit e331d161a29d06a5ada8a6b8c8278759a599a38f.

--- App.js
+++ App.js
@@ -8,13 +8,10 @@
 
 import React from 'react';
 import AppStack from './src/screens/index';
-import Amplify from 'aws-amplify';
-import amplify from './aws-exports';
 
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  Amplify.configure(amplify);
   return (
       <AppStack />
   )
