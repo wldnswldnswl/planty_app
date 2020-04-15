a8de0d5d07962b937902391b8e3942cbf18e754d
maywngml <maywngml@naver.com>
2020-04-15 20:46:24 +0900

    commit

--- src/common/common.js
+++ src/common/common.js
@@ -8,7 +8,7 @@ import {LocaleConfig} from 'react-native-calendars';
 export function setCalendarConfig(){
         LocaleConfig.locales['kr'] = {
             monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
-            // monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+            monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
             dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'],
             dayNamesShort: ['일','월','화.','수','목','금','토'],
             today: '오늘'
